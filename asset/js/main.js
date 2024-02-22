ecb803b3c897e652343cd7bffaa5fc1603e663e5
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-22 17:20:04 +0100

    const gamer and computer

--- asset/js/main.js
+++ asset/js/main.js
@@ -7,7 +7,7 @@ console.log('works')
 //console.log(email);
 
 // controlla che sia nella lista di chi può accedere 
-
+/*
 let emails = ['arya.22@gmail.com', 'arya.33@gmail.com', 'arya.44@gmail.com'];
 let email = 'arya.33@gmail.com';
 let trovata = false;
@@ -19,6 +19,25 @@ for (let i = 0; i < emails.length; i++) {
     }
 }
 
-console.log(trovata); 
+console.log(trovata); */
 
-// genera un numero casuale 
+// genera un numero casuale da 1 a 6 
+
+function generaNumero(){
+    return Math.floor(Math.random()* 6) +1;
+    
+}
+/*
+var Numero = generaNumero();
+
+console.log(Numero) */
+
+// creo due costanti per il giocatore e il computer
+
+const computer = generaNumero();
+
+const gamer = generaNumero();
+
+console.log('computer', computer);
+
+console.log('gamer', gamer);
