191ea3926d20b9914f2dc224d985072e40a467d9
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-22 15:47:23 +0100

    lista array

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,6 +2,23 @@ console.log('works')
 
 // chiedi all'utente la sua email e controlla che sia nella lista 
 
-const email = prompt('write your email');
+//const email = prompt('write your email');
 
-console.log(email);
+//console.log(email);
+
+// controlla che sia nella lista di chi può accedere 
+
+let emails = ['arya.22@gmail.com', 'arya.33@gmail.com', 'arya.44@gmail.com'];
+let email = 'arya.33@gmail.com';
+let trovata = false;
+
+for (let i = 0; i < emails.length; i++) {
+    if (emails[i] === email) {
+        trovata = true;
+        break;
+    }
+}
+
+console.log(trovata); 
+
+// genera un numero casuale 
