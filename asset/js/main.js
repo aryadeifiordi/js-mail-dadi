ba30179797021766a2917e724da058c8be098385
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-22 17:31:51 +0100

    winner

--- asset/js/main.js
+++ asset/js/main.js
@@ -41,3 +41,13 @@ const gamer = generaNumero();
 console.log('computer', computer);
 
 console.log('gamer', gamer);
+
+// verifico chi ha il numero più alto 
+
+if (computer > gamer) {
+    console.log('Il computer ha un numero più alto. Il computer vince!');
+} else if (computer < gamer) {
+    console.log('Il gamer ha un numero più alto. Il gamer vince!');
+} else {
+    console.log('Il computer e il gamer hanno lo stesso numero. È un pareggio!');
+}
